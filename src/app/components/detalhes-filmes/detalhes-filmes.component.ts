5dc42c84926027dd976f0a965dbfe198307fa953
MaCout0 <jkmbint@gmail.com>
2024-10-07 12:30:22 -0300

    Correções

---
+++ src/app/components/detalhes-filmes/detalhes-filmes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalhes-filmes',
+  standalone: true,
+  imports: [],
+  templateUrl: './detalhes-filmes.component.html',
+  styleUrl: './detalhes-filmes.component.scss'
+})
+export class DetalhesFilmesComponent {
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    API_KEY: 'Bearer ec49a45c5417baceb52845c6f37b4e89',
+  };
