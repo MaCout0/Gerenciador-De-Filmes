4ce8aae3652184767d095495508fc73cf76a306f
MaCout0 <jkmbint@gmail.com>
2024-10-07 12:29:12 -0300

    Correção

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ListagemFilmesComponent } from './components/listagem-filmes/listagem-filmes.component';
+
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localePtBr from '@angular/common/locales/pt';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
+  providers: [{ provide: LOCALE_ID, useValue: 'pt-BR' }],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'Gerenciador-De-Filmes';
+  constructor() {
+    registerLocaleData(localePtBr);
+  }
 }
